04655a427e7e732cee665d447d6de9ab5da3fed1
Lucianoquintela0110 <117698113+Lucianoquintela@users.noreply.github.com>
2024-04-08 11:01:26 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # anime-quiz-up
+Site de quiz de anime<br><br>
+- Compativel com dispositvos móveis<br>
+- Tecnologias usada HTML, CSS e JS 
+
 
--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,37 @@
 function verificarResposta() {
-    var opcao = document.querySelector('input[name="animeFavorito"]:checked').value;
+    var opcao1 = document.querySelector('input[name="nome-grand-line"]:checked').value;
+    var opcao2 = document.querySelector('input[name="nome-do-luffy"]:checked').value;
     
-    if (opcao === "resposta1") {
-              
-        document.getElementById("exibirResultado").innerHTML = "Correto"
-        
-        document.getElementById("exibirResultado").style.color = "green";
-        
-       
+
+
+    
+    if ( opcao1 === "resposta1") {        
+        document.getElementById("exibirResultado1").innerHTML = "Correto!"
+        document.getElementById("exibirResultado1").style.color = "green";
+        console.log('test 1');
     } else {
-        document.getElementById("exibirResultado").innerHTML = "Errado"
-        
-        document.getElementById("exibirResultado").style.color = "red";
-              
+        document.getElementById("exibirResultado1").innerHTML = "Errado!"
+        document.getElementById("exibirResultado1").style.color = "FireBrick";
     }
+    
+    if  (opcao2 === 'Monkey-D-luffy'){
+        document.getElementById("exibirResultado2").innerHTML = "Correto!"
+        document.getElementById("exibirResultado2").style.color = "green";
+    } else {
+        document.getElementById("exibirResultado2").innerHTML = "Errado!"
+        document.getElementById("exibirResultado2").style.color = "FireBrick";
+    }
+
+
+  
 }
 
-
+     
+//    else {
+    //  document.getElementById("exibirResultado1").innerHTML = "Errado!"
+   //   document.getElementById("exibirResultado1").style.color = "FireBrick";
+  //    console.log('test 3');     
+//      }
 
 
 
